389123d578d84bf8f0077582ae2f327676a3ddcd
David Levine <levine1726@protonmail.com>
2023-03-09 16:50:35 -0500

    replace hello world with PomodoroTimer component

---
+++ src/components/PomodoroTimer.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+interface Timer {
+  time: string
+}
+
+const timer: Timer = reactive({ time: '25:00' })
+</script>
+
+<template>
+  <h1 class="greetings">Hello!</h1>
+  <h2>{{ timer.time }}</h2>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
